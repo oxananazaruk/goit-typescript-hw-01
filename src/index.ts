c77fb6fad5b012434ea818463ef67c13887b3678
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-12-21 21:36:08 +0200

    Index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
-// function add(num1: number, num2: number) {
-//   return num1 + num2;
-// }
-
-// add(1, 1);
-
 let total: number = 100;
 let names: string = "jey";
 let isActive: boolean = true;
+
+const user: { name: string; age: number } = {
+    name: 'far',
+    age: 20,
+};
+
+type User = {
+    name: string;
+    age: number;
+};
+
+const newUser: User = {
+    name: "fsd",
+    age: 15,
+};
+
+
+function add(num1: number, num2: number): string {
+  return `${num1}` + `{$num2}`;
+}
+
+add(1, 1);
