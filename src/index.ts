06efc5659ae02393bb28c2f3228b825bff522c41
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-12-22 17:28:38 +0200

    debugging

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,37 @@
-let total: number = 100;
-let names: string = "jey";
-let isActive: boolean = true;
+import { concatenation } from './concatenation';
 
-const user: { name: string; age: number } = {
-    name: 'far',
-    age: 20,
-};
+const button = document.querySelector('button')!;
+const input = document.querySelector('input')!;
 
-type User = {
-    name: string;
-    age: number;
-};
-
-const newUser: User = {
-    name: "fsd",
-    age: 15,
-};
-
-
-function add(num1: number, num2: number): string {
-  return `${num1}` + `{$num2}`;
+if (button && input) {
+  button.addEventListener('click', () => {
+    concatenation(input.value, 'hello!');
+  });
 }
 
-add(1, 1);
+// let total: number = 100;
+// let names: string = "jey";
+// let isActive: boolean = true;
+
+// const user: { name: string; age: number } = {
+//     name: 'far',
+//     age: 20,
+// };
+
+// type User = {
+//     name: string;
+//     age: number;
+// };
+
+// const newUser: User = {
+//     name: "fsd",
+//     age: 15,
+// };
+
+
+// function add(num1: number, num2: number): string {
+//   return `${num1}` + `{$num2}`;
+// }
+
+// add(1, 1);
+// const flat: number = 4;
